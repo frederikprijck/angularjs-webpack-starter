1149b37af439bfba156ec635045ed4540cbe906a
Frederik Prijck <frederik.prijck@gmail.com>
2017-08-06 18:13:39 +0200

    fix: Use latest UglifyJs (beta)

--- webpack-prd.config.js
+++ webpack-prd.config.js
@@ -64,28 +64,7 @@ module.exports = {
     },
 
     plugins: [
-        new UglifyJsPlugin({
-            beautify: false,
-            output: {
-                comments: false
-            },
-            mangle: {
-                screw_ie8: true
-            },
-            compress: {
-                screw_ie8: true,
-                warnings: false,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true,
-                negate_iife: false
-            },
-        }),
+        new UglifyJsPlugin(),
         new HtmlWebpackPlugin({
             title: 'AngularJS - Webpack',
             template: 'index.html',
