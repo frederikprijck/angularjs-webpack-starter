6d06ea640370f5752b52ba0af41ff41d0b946d6c
Frederik <frederik.prijck@gmail.com>
2020-10-02 10:02:16 +0200

    chore(setup): Split chunks

--- webpack-prd.config.js
+++ webpack-prd.config.js
@@ -1,21 +1,32 @@
-const path = require('path');
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.config.js');
-const DESTINATION = path.resolve( __dirname, 'public' );
+const path = require("path");
+const webpackMerge = require("webpack-merge");
+const commonConfig = require("./webpack.config.js");
+const DESTINATION = path.resolve(__dirname, "public");
 
 /**
  * Webpack Plugins
  */
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = webpackMerge(commonConfig, {
-    mode: 'production',
-    output: {
-        path: DESTINATION,
-        filename: 'js/[name]-bundle-[chunkhash].js'
+  mode: "production",
+  output: {
+    path: DESTINATION,
+    filename: "js/[name]-bundle-[chunkhash].js",
+  },
+  optimization: {
+    chunkIds: "named",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: "initial",
+          name: "vendor",
+          priority: 10,
+          enforce: true,
+        },
+      },
     },
-
-    plugins: [
-        new UglifyJsPlugin()
-    ]
+  },
+  plugins: [new UglifyJsPlugin()],
 });
